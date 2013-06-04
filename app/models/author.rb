01cf5fb0bc2d3cc4dacc8dd473be638651f84c8e
Adam Fernandez <adampfernandez.com>
2013-06-04 01:21:02 -0400

    Sass and bootstrap

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  attr_accessible :name, :password, :email, :bio, :website, :twitter 
+  attr_accessible :email, :password, :name , :bio, :website, :twitter 
   
   has_many :posts
 end
